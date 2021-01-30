1e24d20b68758e4f474d0544ffd22a872891d6f0
thelvyn2001 <39500779+thelvyn2001@users.noreply.github.com>
2021-01-30 12:22:18 +0100

    Update createTemporaryTable.sql

--- createTemporaryTable.sql
+++ createTemporaryTable.sql
@@ -15,3 +15,38 @@ select * from #person_address
 DROP TABLE #person_address
 
 select * from Person.Address
+
+/* Create ant insert with other tables
+Base AdventureWordk2019 */
+create table #product (
+	productId	int			not null,
+	name		varchar(50) null,
+	number		varchar(25) null ,
+	ListPrice	money		null,
+	idCategory	int			null,
+	category	varchar(50)			null
+)
+
+insert into #product
+select ProductId, Name, ProductNumber, null, ProductSubcategoryID, null
+from Production.Product
+
+
+update #product
+set #product.ListPrice = Product.ListPrice
+from Production.Product
+where #product.productId = Product.ProductID
+
+
+update #product
+set category = pdtCat.Name
+from Production.ProductCategory pdtCat
+where idCategory = pdtCat.ProductCategoryID
+
+select * from #product
+--where name like 'a%'
+
+
+drop table #product
+
+
